c67ed445cdf2df5f6e9883ef13fbf8cf5235c600
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-03-12 04:49:27 +0700

    feat: init review endpoint

---
+++ src/main/java/com/skripsi/siap_sewa/entity/ReviewEntity.java
@@ -0,0 +1,38 @@
+package com.skripsi.siap_sewa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "REVIEW")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "customer_id", nullable = false)
+    private String customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    private ProductEntity product;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    private String image;
+
+    private Double rating;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+}
---
+++ src/main/java/com/skripsi/siap_sewa/service/ReviewService.java
@@ -0,0 +1,9 @@
+package com.skripsi.siap_sewa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+}
