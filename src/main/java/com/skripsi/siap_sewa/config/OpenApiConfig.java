ca068ca3c8b208d6679907fde3022341caede6f8
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-04-18 12:28:07 +0700

    Fix: swagger documentation

--- src/main/java/com/skripsi/siap_sewa/config/OpenApiConfig.java
+++ src/main/java/com/skripsi/siap_sewa/config/OpenApiConfig.java
@@ -2,8 +2,12 @@ package com.skripsi.siap_sewa.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,5 +18,12 @@ public class OpenApiConfig {
                 .info(new Info().title("SIAP SEWA API")
                         .description("API documentation for SIAP SEWA application")
                         .version("1.0"));
+    }
+
+    @Bean
+    public OpenAPI openAPI(ServletContext servletContext) {
+        Server server = new Server().url(servletContext.getContextPath());
+        return new OpenAPI()
+                .servers(List.of(server));
     }
 }
