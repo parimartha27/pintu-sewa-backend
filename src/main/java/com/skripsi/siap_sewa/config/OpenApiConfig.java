0fb2415ec604867dee11596b7f0a729ef11aa6c2
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-04-23 15:42:47 +0700

    Fix: swagger

--- src/main/java/com/skripsi/siap_sewa/config/OpenApiConfig.java
+++ src/main/java/com/skripsi/siap_sewa/config/OpenApiConfig.java
@@ -13,17 +13,14 @@ import java.util.List;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("SIAP SEWA API")
-                        .description("API documentation for SIAP SEWA application")
-                        .version("1.0"));
-    }
-
-    @Bean
-    public OpenAPI openAPI(ServletContext servletContext) {
+    public OpenAPI customOpenAPI(ServletContext servletContext) {
         Server server = new Server().url(servletContext.getContextPath());
+
         return new OpenAPI()
+                .info(new Info()
+                        .title("PINTU SEWA API")
+                        .description("API documentation for PINTU SEWA application")
+                        .version("1.0"))
                 .servers(List.of(server));
     }
 }
