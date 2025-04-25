9feb86aa9a4867d2fc882770f29729fc928e8f1d
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-04-25 22:59:55 +0700

    Add: annotation @Data for mapping data in wallet report response

--- src/main/java/com/skripsi/siap_sewa/dto/wallet/WalletHistoryResponse.java
+++ src/main/java/com/skripsi/siap_sewa/dto/wallet/WalletHistoryResponse.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class WalletHistoryResponse {
     private List<WalletHistory> walletHistory;
 
+    @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
