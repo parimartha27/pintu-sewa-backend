78a3054f04b56d372cbe8ab903a14eebe6cb5318
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-06-18 02:41:13 +0700

    Update ChatHeaderRepository.java

--- src/main/java/com/skripsi/siap_sewa/repository/ChatHeaderRepository.java
+++ src/main/java/com/skripsi/siap_sewa/repository/ChatHeaderRepository.java
@@ -1,12 +1,10 @@
 package com.skripsi.siap_sewa.repository;
 
-import com.skripsi.siap_sewa.entity.chat.ChatDetailEntity;
 import com.skripsi.siap_sewa.entity.chat.ChatHeaderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
