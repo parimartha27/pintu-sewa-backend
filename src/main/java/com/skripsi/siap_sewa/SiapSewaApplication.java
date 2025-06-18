622713981b69d70b11827e6c14da51accc11fbae
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-06-18 23:43:05 +0700

    Revert "FIx: timezone"
    This reverts commit 17690d28f00259e64dcc6a2af3ae3cb5bb8cb21d.

--- src/main/java/com/skripsi/siap_sewa/SiapSewaApplication.java
+++ src/main/java/com/skripsi/siap_sewa/SiapSewaApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.TimeZone;
-
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
 public class SiapSewaApplication {
 
 	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
 		SpringApplication.run(SiapSewaApplication.class, args);
 	}
 
